2231dbf24f1a5b58a01fcaf3ec63531d6d8aff30
victor-gabriele <134950739+victor-gabriele@users.noreply.github.com>
2023-09-20 12:19:00 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function tocaSomPom(){
 }
 function tocaSomClap(){
     document.querySelector('#som_tecla_clap').play();
-    document.querySelector('.tecla_pom_clap').onclick = tocaSomClap;
+    document.querySelector('.tecla_clap').onclick = tocaSomClap;
 }
 function tocaSomTim(){
     document.querySelector('#som_tecla_tim').play();
@@ -35,3 +35,13 @@ function tocaSomTom(){
     document.querySelector('.tecla_tom').onclick = tocaSomTom;
 }
 document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
+listaDeTeclas[0].onclick = tocaSomPom;
+listaDeTeclas[1].onclick = tocaSomClap;
+listaDeTeclas[2].onclick = tocaSomTim;
+listaDeTeclas[3].onclick = tocaSomPuff;
+listaDeTeclas[4].onclick = tocaSomSplash;
+listaDeTeclas[5].onclick = tocaSomToim;
+listaDeTeclas[6].onclick = tocaSomPsh;
+listaDeTeclas[7].onclick = tocaSomTic;
+listaDeTeclas[8].onclick = tocaSomTom;
