dbe7e02dae08e9930b86ce35464e33792c81f6d3
victor-gabriele <134950739+victor-gabriele@users.noreply.github.com>
2023-09-25 12:23:03 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -36,12 +36,12 @@ function tocaSomTom(){
 }
 document.querySelectorAll('.tecla');
 const listaDeTeclas = document.querySelectorAll('.tecla');
-listaDeTeclas[0].onclick = tocaSomPom;
-listaDeTeclas[1].onclick = tocaSomClap;
-listaDeTeclas[2].onclick = tocaSomTim;
-listaDeTeclas[3].onclick = tocaSomPuff;
-listaDeTeclas[4].onclick = tocaSomSplash;
-listaDeTeclas[5].onclick = tocaSomToim;
-listaDeTeclas[6].onclick = tocaSomPsh;
-listaDeTeclas[7].onclick = tocaSomTic;
-listaDeTeclas[8].onclick = tocaSomTom;
+listaDeTeclas[contador].onclick = tocaSomPom;
+let contador = 0;
+
+//Estrutura de repetição-Enquanto
+while(contador < listaDeTeclas.length) {
+
+contador = contador + 1;
+
+}
